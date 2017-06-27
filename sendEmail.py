a273120bbafbc6fe970e6a1372ff8ac90908cb66
jemonsivais <jemonsivais@gmail.com>
2017-06-26 18:42:35 -0600

    Generalized input.

--- sendEmail.py
+++ sendEmail.py
@@ -7,29 +7,29 @@ from email.mime.text import MIMEText
 from email.MIMEBase import MIMEBase
 from email import Encoders
 
-def sendEmail():
-    fromaddr = "fromaddr"
-    toaddr = "toaddr"
-
+def sendEmail(addr, passwd, server):
+    ##Construct message
     msg = MIMEMultipart()
-    msg['From']=fromaddr
-    msg['To']=toaddr
+    msg['From']=addr
+    msg['To']=addr
     msg['Subject']="Motion was detected in your room!"
     body = "There was motion detected in your room on "+datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p")
     msg.attach(MIMEText(body,'plain'))
 
+    ##Attach email
     filename = 'feed.avi'
     fo = open(filename,"rb")
     att = email.mime.application.MIMEApplication(fo.read(),_subtype="avi")
     fo.close()
     att.add_header('Content-Disposition', 'attachment', filename=filename)
     msg.attach(att)
-    
-    server = smtplib.SMTP('smtp.gmail.com', 587)
+
+    ##Send email
+    server = smtplib.SMTP(server, 587)
     server.ehlo()
     server.starttls()
     server.ehlo()
-    server.login(fromaddr, 'pass')
+    server.login(addr, passwd)
     text = msg.as_string()
-    server.sendmail(fromaddr, toaddr, text)
+    server.sendmail(addr, addr, text)
     
